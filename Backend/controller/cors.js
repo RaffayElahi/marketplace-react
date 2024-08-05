37e34b8d79d95b2519600e33e3eee403e8b2110e
Raffay Elahi <honeybee33392@gmail.com>
2024-08-05 23:28:52 +0500

    Updated cors for prod

--- Backend/controller/cors.js
+++ Backend/controller/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL, 
   credentials: true, // Allow credentials (cookies) to be sent
   methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allow these HTTP methods
   allowedHeaders: 'Content-Type,Authorization', // Allow these headers
