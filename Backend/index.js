4b86fe236a948355f6e7c5dd54e9a9afae600ad1
Raffay Elahi <honeybee33392@gmail.com>
2024-08-06 00:12:40 +0500

    Debugging

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const server = express();
 require('dotenv').config();
 
 
-server.use(cors(corsOptions));
+// server.use(cors(corsOptions));
 
 // Middleware
 server.use(bodyParser.json());
@@ -26,7 +26,7 @@ server.use(cookieParser());
 
 server.use(securityMiddleware);
 
-// Apply routes
+
 server.get('/', (req, res) => {
     res.send('Hello');
 });
