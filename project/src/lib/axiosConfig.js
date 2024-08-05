019bb42650d7337127b05084eca81b0d764be37c
Raffay Elahi <honeybee33392@gmail.com>
2024-08-05 23:53:31 +0500

    added hosted backend route

--- project/src/lib/axiosConfig.js
+++ project/src/lib/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://marketplace-react-api.vercel.app/';
 
 const axiosConfig = axios.create({
     baseURL: BASE_URL,
