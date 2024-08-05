80679fb1758e963bdbecaff78ab55eae1985a439
Raffay Elahi <honeybee33392@gmail.com>
2024-08-06 00:42:08 +0500

    Production

--- Backend/controller/cors.js
+++ Backend/controller/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: 'https://marketplace-react-kappa.vercel.app', 
+  origin: ['https://marketplace-react-kappa.vercel.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
